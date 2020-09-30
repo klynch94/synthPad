ce237af0f450b7ad353551bd7909ebf64aaf32ac
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-29 20:12:09 -0500

    added presskey hook

--- react-synthpad/src/components/synth/index.js
+++ react-synthpad/src/components/synth/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DuoSynth, FMSynth } from 'tone';
 import { useAppContext } from '../../context';
 import Pad from './pad'
@@ -12,8 +12,18 @@ const styles = {
 export default () => {
     const { state } = useAppContext()
 
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown)
+        return() => window.removeEventListener('keydown', handleKeyDown)
+    }, [state.mode])
+
     const synth = state.mode === 'light' ? new FMSynth() : new DuoSynth()
     synth.toMaster();
+
+    const handleKeyDown = e => {
+        console.log(e);
+        playSound(e.key)
+    }
 
     const playSound = targetLetter => {
         const foundNote = state.notes.find(({letter}) => letter === targetLetter)
